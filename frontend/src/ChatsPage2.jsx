70f21bfa5da924257e3f8eeb6292a5172aa81a68
Yacinekermame <86516881+kaaado@users.noreply.github.com>
2024-07-04 20:54:47 +0100

    Update ChatsPage2.jsx

--- frontend/src/ChatsPage2.jsx
+++ frontend/src/ChatsPage2.jsx
@@ -1,7 +1,7 @@
 import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
 
 const ChatsPage = (props) => {
-  const projectId = "2a4a4059-1c32-4a5f-ae13-cabd38fc0209";
+  const projectId = 'enter your project id';
   const chatProps = useMultiChatLogic(projectId, props.user.username, props.user.secret);
 
   return (
